9e898c433fc76fa0797052e5938a49e4549fdd5e
woodiny <woodiny.dev@gmail.com>
2024-07-01 21:49:30 +0900

    🔧 add schema
    - connections table

--- sql/schema.sql
+++ sql/schema.sql
@@ -11,3 +11,17 @@ create table users (
     primary key (seq),
     constraint unq_user_email unique (email)
 );
+
+-- connections
+drop table if exists connections;
+
+create table connections (
+    seq bigint not null auto_increment,
+    user_seq bigint not null,
+    target_seq bigint not null,
+    granted_at datetime default null,
+    create_at datetime not null default current_timestamp(),
+    primary key (seq),
+    constraint fk_connection_to_user foreign key (user_seq) references users (seq) on delete restrict on update restrict,
+    constraint fk_connection_to_user2 foreign key (target_seq) references users (seq) on delete restrict on update restrict
+);
