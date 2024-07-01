d72de5bec36900a6afad331b5a3604f83a3fdf99
woodiny <woodiny.dev@gmail.com>
2024-07-01 21:36:50 +0900

    ✅ user update 테스트
    - email update
    - passwd update

--- src/test/java/woodiny/socialserver/service/UserServiceTest.java
+++ src/test/java/woodiny/socialserver/service/UserServiceTest.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 @Transactional
@@ -20,19 +21,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserServiceTest {
     @Autowired
     private UserService userService;
-
-    @Test
-    public void registerNewUser_mustReturnValidUserId() {
-        Email email = new Email("tester1@gmail.com");
-        String passwd = "1234";
-
-        long userId = userService.register(email, passwd);
-        Optional<User> findUser = userService.findByUserId(userId);
-
-        assertTrue(findUser.isPresent());
-        assertThat(userId).isGreaterThan(0);
-        assertThat(userId).isEqualTo(findUser.get().getSeq());
-    }
 
     @Test
     public void findByUserId() {
@@ -56,5 +44,84 @@ class UserServiceTest {
 
         assertTrue(findUser.isPresent());
         assertThat(userId).isEqualTo(findUser.get().getSeq());
+    }
+
+    @Test
+    public void registerNewUser_mustReturnValidUserId() {
+        Email email = new Email("tester1@gmail.com");
+        String passwd = "1234";
+
+        long userId = userService.register(email, passwd);
+        Optional<User> findUser = userService.findByUserId(userId);
+
+        assertTrue(findUser.isPresent());
+        assertThat(userId).isGreaterThan(0);
+        assertThat(userId).isEqualTo(findUser.get().getSeq());
+    }
+
+    @Test
+    public void updateEmail() {
+        Email email = new Email("tester1@gmail.com");
+        String passwd = "1234";
+        long userId = userService.register(email, passwd);
+        User findUser = userService.findByUserId(userId).get();
+        
+        Email newEmail = new Email("newTester1@gmail.com");
+
+        User user = new User(
+                findUser.getSeq(),
+                newEmail,
+                findUser.getPasswd(),
+                findUser.getLoginCount(),
+                findUser.getLastLoginAt(),
+                findUser.getCreateAt());
+        userService.update(user);
+
+        User updatedUser = userService.findByUserId(userId).get();
+
+        assertThat(updatedUser).isEqualTo(findUser);
+        assertThat(updatedUser.getEmail().getAddress())
+                .isEqualTo(newEmail.getAddress());
+        assertThat(updatedUser.getPasswd())
+                .isEqualTo(findUser.getPasswd());
+        assertThat(updatedUser.getLoginCount())
+                .isEqualTo(findUser.getLoginCount());
+        assertThat(updatedUser.getLastLoginAt())
+                .isEqualTo(findUser.getLastLoginAt());
+        assertThat(updatedUser.getCreateAt())
+                .isEqualTo(findUser.getCreateAt());
+    }
+
+    @Test
+    public void updatePassword() {
+        Email email = new Email("tester1@gmail.com");
+        String passwd = "1234";
+        long userId = userService.register(email, passwd);
+        User findUser = userService.findByUserId(userId).get();
+
+        String newPasswd = "5678";
+
+        User user = new User(
+                findUser.getSeq(),
+                findUser.getEmail(),
+                newPasswd,
+                findUser.getLoginCount(),
+                findUser.getLastLoginAt(),
+                findUser.getCreateAt());
+        userService.update(user);
+
+        User updatedUser = userService.findByUserId(userId).get();
+
+        assertThat(updatedUser).isEqualTo(findUser);
+        assertThat(updatedUser.getEmail().getAddress())
+                .isEqualTo(findUser.getEmail().getAddress());
+        assertThat(updatedUser.getPasswd())
+                .isEqualTo(newPasswd);
+        assertThat(updatedUser.getLoginCount())
+                .isEqualTo(findUser.getLoginCount());
+        assertThat(updatedUser.getLastLoginAt())
+                .isEqualTo(findUser.getLastLoginAt());
+        assertThat(updatedUser.getCreateAt())
+                .isEqualTo(findUser.getCreateAt());
     }
 }
