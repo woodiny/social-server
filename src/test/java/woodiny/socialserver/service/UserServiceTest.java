01fa5c27289766ba1ffa257bffde0a488beebcae
woodiny <woodiny.dev@gmail.com>
2024-07-02 22:46:45 +0900

    ⚰️ unused import 제거

--- src/test/java/woodiny/socialserver/service/UserServiceTest.java
+++ src/test/java/woodiny/socialserver/service/UserServiceTest.java
@@ -1,7 +1,6 @@
 package woodiny.socialserver.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,7 +11,6 @@ import woodiny.socialserver.model.user.User;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
