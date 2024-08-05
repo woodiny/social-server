0170364fc989642ffb037cffa7b0d04d11894e09
woodiny <woodiny.dev@gmail.com>
2024-08-05 22:08:22 +0900

    ♻️ Post 기능 관련 모델 수정
    - Getter 메서드 추가
    - 생성자 추가

--- src/main/java/woodiny/socialserver/model/post/Post.java
+++ src/main/java/woodiny/socialserver/model/post/Post.java
@@ -1,18 +1,30 @@
 package woodiny.socialserver.model.post;
 
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
 public class Post {
-    private final Long seq;
-    private final String userId;
+    private Long seq;
+
+    @Getter
+    private final Long userId;
+
+    @Getter
     private String contents;
+
+    @Getter
     private int likes;
     private boolean likesOfMe;
+
+    @Getter
     private int comments;
     private final Writer writer;
-    private final LocalDateTime createAt;
 
-    public Post(Long seq, String userId, String contents, int likes, boolean likesOfMe, int comments, Writer writer, LocalDateTime createAt) {
+    @Getter
+    private LocalDateTime createAt;
+
+    public Post(Long seq, Long userId, String contents, int likes, boolean likesOfMe, int comments, Writer writer, LocalDateTime createAt) {
         this.seq = seq;
         this.userId = userId;
         this.contents = contents;
@@ -21,5 +33,21 @@ public class Post {
         this.comments = comments;
         this.writer = writer;
         this.createAt = createAt;
+    }
+
+    public Post(Long seq, Long userId, String contents, int likes, Writer writer, LocalDateTime createAt) {
+        this.seq = seq;
+        this.userId = userId;
+        this.contents = contents;
+        this.likes = likes;
+        this.writer = writer;
+        this.createAt = createAt;
+    }
+
+    public Post(Long userId, String contents, Writer writer) {
+        this.userId = userId;
+        this.contents = contents;
+        this.writer = writer;
+        this.createAt = LocalDateTime.now();
     }
 }
--- src/main/java/woodiny/socialserver/model/post/Writer.java
+++ src/main/java/woodiny/socialserver/model/post/Writer.java
@@ -8,6 +8,10 @@ public class Writer {
     private final Email email;
     private final String name;
 
+    public Writer(Email email) {
+        this(email, null);
+    }
+
     public Writer(Email email, String name) {
         this.email = email;
         this.name = name;
