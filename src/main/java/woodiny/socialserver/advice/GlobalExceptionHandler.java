90e9ca5b08f91e42e8de9a7fc549fc223b7b8ac5
woodiny <woodiny.dev@gmail.com>
2024-08-06 22:16:09 +0900

    ✨ 에러 발생시 응답을 위한 Advice 구현

---
+++ src/main/java/woodiny/socialserver/advice/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package woodiny.socialserver.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, Object>> handlerRuntimeException(RuntimeException e) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("message", e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
