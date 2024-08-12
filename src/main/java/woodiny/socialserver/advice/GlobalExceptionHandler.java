a85e5a8d9c697c8bf46e4e655795374f9a3b2628
woodiny <woodiny.dev@gmail.com>
2024-08-12 22:02:59 +0900

    🎨 ExceptionHandler 개선
    - ControllerAdvice -> RestControllerAdvice
    - 응답을 ApiResponse 로 처리

--- src/main/java/woodiny/socialserver/advice/GlobalExceptionHandler.java
+++ src/main/java/woodiny/socialserver/advice/GlobalExceptionHandler.java
@@ -1,37 +1,29 @@
 package woodiny.socialserver.advice;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import woodiny.socialserver.controller.ApiResponse;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Map<String, Object>> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("timestamp", LocalDateTime.now());
-        response.put("status", HttpStatus.BAD_REQUEST.value());
-        response.put("message", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    public ApiResponse<Object> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ApiResponse.ERROR(
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                HttpStatus.BAD_REQUEST
+        );
     }
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Map<String, Object>> handlerRuntimeException(RuntimeException e) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("timestamp", LocalDateTime.now());
-        response.put("status", HttpStatus.BAD_REQUEST.value());
-        response.put("message", e.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    public ApiResponse<Object> handlerRuntimeException(RuntimeException e) {
+        return ApiResponse.ERROR(
+                e.getMessage(),
+                HttpStatus.BAD_REQUEST
+        );
     }
 }
